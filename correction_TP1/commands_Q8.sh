944e662b6f70705185969e698762169642f7cb8d
Nicolas <alcalan@iarc.fr>
2021-10-27 09:54:01 +0200

    Delete correction_TP1 directory

--- correction_TP1/commands_Q8.sh
+++
@@ -1,6 +0,0 @@
-wget http://refgenomes.databio.org/v2/asset/hg38/salmon_partial_sa_index/archive?tag=default salmon_sa_default.tgz
-tar -xzvf salmon_sa_default.tgz
-
-nextflow run SalmonSRA.nf -c nextflow.config -profile conda \
-    --ref /home/nalcala/Medical_Genomics_TP1/data_test/REF/17.fasta
-
