937aaf05c44aede5ad98e48327e4e75abfa6ed84
Nicolas <alcalan@iarc.fr>
2021-11-16 17:21:53 +0100

    Update install.R

--- install.R
+++ install.R
@@ -2,6 +2,8 @@
 install.packages("tidyverse")
 
 ## BioC dependencies
-
 install.packages("BiocManager")
 BiocManager::install(c("MOFA2"))
+system("pip install mofapy2")
+library(reticulate)
+use_python(system("which python",intern=T), required=TRUE)
