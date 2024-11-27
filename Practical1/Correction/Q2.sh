fccf50bf9977c1c868232fbe0d1e2a35f7d93dda
Nicolas <alcalan@iarc.fr>
2024-11-27 14:25:18 +0100

    clean previous practicals and add lectures

--- Practical1/Correction/Q2.sh
+++
@@ -1,6 +0,0 @@
-#a) 
-mkdir Practical1
-cd Practical1
-git clone https://github.com/IARCbioinfo/data_test.git
-
-#b) Real fastq files are several Gb large and testing software would be very long. A small data test set allows to quickly test if command lines and the general workflow commands can be executed without error.
--- Practical2/MOFA2_test.R
+++
@@ -1,9 +0,0 @@
-library(MOFA2)
-file <- system.file("extdata", "test_data.RData", package = "MOFA2")
-load(file)
-# Create the MOFA object
-MOFAmodel <- create_mofa(dt)
-# Prepare the MOFA object with default options
-MOFAmodel <- prepare_mofa(MOFAmodel)
-# Run the MOFA model
-MOFAmodel <- run_mofa(MOFAmodel)
